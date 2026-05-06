2dfaff76f6553cbf79d13c55c789551d02d5b27c
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2026-05-06 06:14:53 +0000

    deploy

--- weather-app/src/services/api.js
+++ weather-app/src/services/api.js
@@ -1,9 +1,9 @@
 export async function getWeather(city) {
-  // const apiKey = import.meta.env.VITE_API_KEY;
-  // TODO: mover API key para variável de ambiente antes do deploy
-const apiKey = "6615e7076b26d2816962d95362760b29";
+  const apiKey = import.meta.env.VITE_API_KEY;
 
-  // console.log("API KEY:", apiKey);
+  if (!apiKey) {
+    throw new Error("API key não configurada");
+  }
 
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`
